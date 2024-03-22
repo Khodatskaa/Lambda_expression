f84afc3df34082230c4ceb832fa300fa7ecd6c46
Khodatskaa <khodatskaalina@gmail.com>
2024-03-22 19:45:30 +0200

    Ex 4 was done

--- Lambda_expression/Program.cs
+++ Lambda_expression/Program.cs
@@ -4,6 +4,13 @@
     {
         static void Main()
         {
+            Func<DateTime, bool> isProgrammersDay = date => date.DayOfYear == 256;
+
+            DateTime testDate1 = new DateTime(2024, 9, 12); 
+            DateTime testDate2 = new DateTime(2024, 9, 13); 
+
+            Console.WriteLine($"{testDate1.ToShortDateString()} is Programmer's Day: {isProgrammersDay(testDate1)}");
+            Console.WriteLine($"{testDate2.ToShortDateString()} is Programmer's Day: {isProgrammersDay(testDate2)}");
         }
     }
 }
