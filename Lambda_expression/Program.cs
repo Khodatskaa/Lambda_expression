9364c4c3cd35101efd7b075efab36ceb1d5ab4f1
Khodatskaa <khodatskaalina@gmail.com>
2024-03-22 19:52:36 +0200

    Ex 6 was done

--- Lambda_expression/Program.cs
+++ Lambda_expression/Program.cs
@@ -4,6 +4,22 @@
     {
         static void Main()
         {
+            Func<int[], int> findMin = arr =>
+            {
+                int min = arr[0];
+                foreach (int num in arr)
+                {
+                    if (num < min)
+                    {
+                        min = num;
+                    }
+                }
+                return min;
+            };
+
+            int[] numbers = { 5, 8, 9, 11, 3, 3, 55 };
+
+            Console.WriteLine($"Minimal value in the array: {findMin(numbers)}");
         }
     }
 }
