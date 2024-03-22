5d1dd6f6f1a4dd4cfc8bc1715eff0f36b3beeaa6
Khodatskaa <khodatskaalina@gmail.com>
2024-03-22 19:49:43 +0200

    Ex 5 was done

--- Lambda_expression/Program.cs
+++ Lambda_expression/Program.cs
@@ -4,6 +4,22 @@
     {
         static void Main()
         {
+            Func<int[], int> findMax = arr =>
+            {
+                int max = arr[0];
+                foreach (int num in arr)
+                {
+                    if (num > max)
+                    {
+                        max = num;
+                    }
+                }
+                return max;
+            };
+
+            int[] numbers = { 5, 8, 9, 11, 3, 3, 55 };
+
+            Console.WriteLine($"Maximum value in the array: {findMax(numbers)}");
         }
     }
 }
