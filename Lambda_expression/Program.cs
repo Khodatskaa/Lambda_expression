c4f818c05355b42f1b0e594f4da427b870a719fd
Khodatskaa <khodatskaalina@gmail.com>
2024-03-22 19:56:06 +0200

    Ex 7 was done

--- Lambda_expression/Program.cs
+++ Lambda_expression/Program.cs
@@ -4,6 +4,22 @@
     {
         static void Main()
         {
+            Func<int[], int[]> findOddNumbers = (numbers) =>
+            {
+                List<int> oddNumbers = new List<int>();
+                foreach (var number in numbers)
+                {
+                    if (number % 2 != 0)
+                    {
+                        oddNumbers.Add(number);
+                    }
+                }
+                return oddNumbers.ToArray();
+            };
+
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            int[] oddNumbers = findOddNumbers(numbers);
+            Console.WriteLine(string.Join(", ", oddNumbers));
         }
     }
 }
