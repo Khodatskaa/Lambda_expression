0f62834a5e090084065f4cdaf88d383162473324
Khodatskaa <khodatskaalina@gmail.com>
2024-03-22 20:38:12 +0200

    Ex 4 was done

--- Lambda_expression/Program.cs
+++ Lambda_expression/Program.cs
@@ -4,6 +4,12 @@
     {
         static void Main()
         {
+            Func<int[], int> countPositiveNumbers = arr => arr.Count(num => num > 0);
+
+            int[] numbers = { -3, 7, -2, 9, -5, 1, 0, 6 };
+
+            int count = countPositiveNumbers(numbers);
+            Console.WriteLine($"Number of positive numbers in the array: {count}");
         }
     }
 }
