c8de25056a78988de7605b5587f260e80c4c911f
Khodatskaa <khodatskaalina@gmail.com>
2024-03-22 19:09:17 +0200

    Ex 2 was done

--- Lambda_expression/Program.cs
+++ Lambda_expression/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main()
         {
-            
+            Func<int, int, int> countSquareNumbers = delegate (int start, int end)
+            {
+                int count = 0;
+                for (int i = start; i <= end; i++)
+                {
+                    int squareRoot = (int)Math.Sqrt(i);
+                    if (squareRoot * squareRoot == i)
+                    {
+                        count++;
+                    }
+                }
+                return count;
+            };
+
+            int startRange = 1;
+            int endRange = 100;
+
+            int squareCount = countSquareNumbers(startRange, endRange);
+            Console.WriteLine($"Number of square numbers between {startRange} and {endRange}: {squareCount}");
         }
     }
 }
