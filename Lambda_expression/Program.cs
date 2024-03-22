5edc92df4aba9f7bcdc2308bba0f7542c827f25b
Khodatskaa <khodatskaalina@gmail.com>
2024-03-22 18:55:00 +0200

    Ex 1 was done

--- Lambda_expression/Program.cs
+++ Lambda_expression/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main()
         {
-            
+            Func<int, bool> isEven = delegate (int num)
+            {
+                return num % 2 == 0;
+            };
+
+            int number1 = 10;
+            int number2 = 15;
+
+            Console.WriteLine($"{number1} is {(isEven(number1) ? "even" : "odd")}");
+            Console.WriteLine($"{number2} is {(isEven(number2) ? "even" : "odd")}");
         }
     }
 }
