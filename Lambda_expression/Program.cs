4c81fca3f93c5ee9e30b81e8573e69ff82912063
Khodatskaa <khodatskaalina@gmail.com>
2024-03-22 19:41:52 +0200

    Ex 3 was done

--- Lambda_expression/Program.cs
+++ Lambda_expression/Program.cs
@@ -3,7 +3,12 @@
     internal class Program
     {
         static void Main()
-        {
+        { 
+            Func<int, int> cube = x => x * x * x;
+
+            int number = 5;
+            int result = cube(number);
+            Console.WriteLine($"Cube of {number}: {result}");
         }
     }
 }
