fc861249813bc8c5f7feb70ed4d2fbb8d50ff788
Khodatskaa <khodatskaalina@gmail.com>
2024-03-22 20:22:14 +0200

    Ex 3 was done

--- Lambda_expression/Program.cs
+++ Lambda_expression/Program.cs
@@ -4,6 +4,12 @@
     {
         static void Main()
         {
+            Func<int[], int> countMultiplesOfSeven = arr => arr.Count(num => num % 7 == 0);
+
+            int[] numbers = { 7, 14, 21, 28, 35, 42, 49, 5, 12, 19 };
+
+            int count = countMultiplesOfSeven(numbers);
+            Console.WriteLine($"Number of multiples of seven in the array: {count}");
         }
     }
 }
