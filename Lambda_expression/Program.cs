53bf7601a523d871ecd1a31bab4cf6c42e569869
Khodatskaa <khodatskaalina@gmail.com>
2024-03-22 20:41:34 +0200

    Ex 5 was done

--- Lambda_expression/Program.cs
+++ Lambda_expression/Program.cs
@@ -4,6 +4,19 @@
     {
         static void Main()
         {
+            Action<int[]> displayUniqueNegativeNumbers = arr =>
+            {
+                var uniqueNegativeNumbers = arr.Where(num => num < 0).Distinct();
+                Console.WriteLine("Unique negative numbers in the array:");
+                foreach (var num in uniqueNegativeNumbers)
+                {
+                    Console.WriteLine(num);
+                }
+            };
+
+            int[] numbers = { -3, 7, -2, 9, -5, 1, -3, 0, -2 };
+
+            displayUniqueNegativeNumbers(numbers);
         }
     }
 }
